c266a9f6030df58d01e02a20c92b1df05db75777
Lucky <luckysolanki902@gmail.com>
2024-11-19 18:39:38 +0530

    few minor changes in customercard

--- src/components/cards/CustomerCard.js
+++ src/components/cards/CustomerCard.js
@@ -31,7 +31,7 @@ const CustomerCard = ({ order, expanded, handleChange }) => {
             </AccordionSummary>
             <AccordionDetails style={{ display: 'flex', flexDirection: 'column', textAlign: 'left', padding: '10px' }}>
                 {/* Payment and shipping status */}
-                {order.status && (
+                
                     <div style={{ margin: 0, padding: 0, textAlign: 'left' }}>
                         <Timeline sx={{
                             [`& .${timelineItemClasses.root}:before`]: {
@@ -60,7 +60,7 @@ const CustomerCard = ({ order, expanded, handleChange }) => {
                             </TimelineItem>
                         </Timeline>
                     </div>
-                )}
+                
 
                 {/* Loop through the items in the order */}
                 {order.items.map((item, index) => (
